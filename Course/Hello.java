2e84a707189d0a59acb275af006978b811381bca
kalyan ram <kalyan799ram@gmail.com>
2026-01-21 22:26:48 +0530

    added Hello.java

---
+++ Course/Hello.java
@@ -0,0 +1,13 @@
+class One {
+    public void display() {
+        System.out.println("Hello World");
+    }
+}
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        One ob=new One();
+        ob.display();
+    }
+}
