5db6ee6136c607c2a519688cc1e2ee9d45fb5a9c
--global <kalyan799ram@gmail.com>
2026-01-29 07:55:03 +0530

    Final with constructor Demo code

---
+++ Day-10/FinalCon.java
@@ -0,0 +1,12 @@
+class Ab{
+    // final Ab(){
+    //     System.out.println("In Ab class")
+    // }
+}
+
+public class FinalCon{
+    public static void main(String[] args){
+
+        Ab a=new Ab();
+    }
+}
