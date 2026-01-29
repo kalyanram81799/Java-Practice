1aa22d9b6fa29e64b9875cce36f3920e081db545
--global <kalyan799ram@gmail.com>
2026-01-29 07:57:40 +0530

    Static with final Demo code

---
+++ Day-10/StaticFinal.java
@@ -0,0 +1,9 @@
+class MathConst {
+    static final double PI = 3.14;
+}
+
+public class StaticFinal {
+    public static void main(String[] args) {
+        System.out.println(MathConst.PI);
+    }
+}
