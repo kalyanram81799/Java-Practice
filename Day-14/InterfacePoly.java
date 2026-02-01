e47f0d5f5f1aca061e4fc0810059edd29e2f3f35
--global <kalyan799ram@gmail.com>
2026-02-02 00:30:19 +0530

    added polymorphism code in interface

---
+++ Day-14/InterfacePoly.java
@@ -0,0 +1,27 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class InterfacePoly {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.draw();
+
+        s = new Rectangle();
+        s.draw();
+    }
+}
