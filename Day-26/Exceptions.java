92afd61a50512b574f18cbfa3d8f8870f77bd083
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-15 01:25:33 +0530

    added code for exception

---
+++ Day-26/Exceptions.java
@@ -0,0 +1,7 @@
+public class Exceptions {
+    public static void main(String[] args) throws InterruptedException
+    {
+        Thread.sleep(10000);
+        System.out.println("Hello Geeks");
+    }
+}
