cd6a83597e36da561df81bb96165c255665e57fb
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-14 13:25:13 +0530

    added code inner class files creation demo

---
+++ Day-25/Files.java
@@ -0,0 +1,13 @@
+public class Files {
+    public static void main(String[] args) {
+        class Outer {
+            class Inner {
+                class InInner {
+                    class TwoInner {
+
+                    }
+                }
+            }
+        }
+    }
+}
