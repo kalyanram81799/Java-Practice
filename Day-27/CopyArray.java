27c1f3aa13e2a06a00fdc517ed9e367236588ee4
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-16 00:01:39 +0530

    added code for CopyArray in java

---
+++ Day-27/CopyArray.java
@@ -0,0 +1,14 @@
+public class CopyArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+        int[] copy = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+
+        for (int num : copy)
+            System.out.print(num + " ");
+    }
+}
