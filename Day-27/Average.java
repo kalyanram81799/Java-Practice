08ba1e6501b74e58a1a1beaa6559507cc904c42b
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-16 00:01:16 +0530

    added code to find average

---
+++ Day-27/Average.java
@@ -0,0 +1,15 @@
+public class Average {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        double avg =  sum / arr.length;
+
+        System.out.println("Average = " + avg);
+    }
+}
