840541e8ec5b8e2cd157d07e3e5672d0cd596582
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-17 00:06:28 +0530

    added code for string pool demonstration

---
+++ Day-28/StringPoolMemoryTest.java
@@ -0,0 +1,13 @@
+public class StringPoolMemoryTest {
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
+        String s4 = s3.intern();
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s4);
+    }
+}
