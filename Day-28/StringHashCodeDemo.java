b6098466bfaa60ff68d6f7ff9cfa703d1913b981
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-17 00:08:48 +0530

    added code for StringHashCodeDemonstration

---
+++ Day-28/StringHashCodeDemo.java
@@ -0,0 +1,10 @@
+public class StringHashCodeDemo {
+    public static void main(String[] args) {
+
+        String s = "Java";
+
+        System.out.println(s.hashCode());
+        System.out.println(s.hashCode());
+        System.out.println(s.hashCode());
+    }
+}
