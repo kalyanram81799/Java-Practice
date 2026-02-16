2162f06ab08b566325f10c9027f5c1dee82f63e7
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-17 00:06:01 +0530

    added code for reversing string with logic

---
+++ Day-28/StringReverseWithoutMethod.java
@@ -0,0 +1,10 @@
+public class StringReverseWithoutMethod {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println("the reversed string is:" + reversed);
+    }
+}
