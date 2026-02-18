3c0a78df3bd65406e23e3821691695c3d9352b25
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-19 02:06:34 +0530

    added code for CurrentDateUsingLocalDate

---
+++ Day-30/CurrentDateUsingLocalDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class CurrentDateUsingLocalDate {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today: " + today);
+    }
+}
