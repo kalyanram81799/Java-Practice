aec977991c1b3f4e89ff8bbd13b75355627ada4e
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-19 02:06:57 +0530

    added code for CurrentDateUsingDate

---
+++ Day-30/CurrentDateUsingDate.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class CurrentDateUsingDate {
+    public static void main(String[] args) 
+    {
+        Date d = new Date();
+        System.out.println("Current Date: " + d);
+    }
+}
