c72dc8cd25a8b5727ec46504c1c211f351a0ab39
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-21 21:23:43 +0530

    added code for ThreadYieldExample

---
+++ Day-32/ThreadYieldExample.java
@@ -0,0 +1,18 @@
+class YieldThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+            Thread.yield(); // Hint to scheduler
+        }
+    }
+}
+
+public class ThreadYieldExample {
+    public static void main(String[] args) {
+        YieldThread t1 = new YieldThread();
+        YieldThread t2 = new YieldThread();
+
+        t1.start();
+        t2.start();
+    }
+}
