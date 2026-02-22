15a2b6676dba47e951008cda9ec205b1d03554ad
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-02-22 22:55:16 +0530

    added code for SumUsingReduce

---
+++ Day-33/SumUsingReduce.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SumUsingReduce {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+
+        int sum =
+            list.stream()
+                .reduce(0, (a,b) -> a + b);
+
+        System.out.println(sum);
+    }
+}
